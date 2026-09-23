2251562c46c5ec59eccbf06165a385bed4740ca7
Nessrine <sayarinessrine589@gmail.com>
2026-09-23 17:55:12 +0100

    Add Student component and data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+// import { RouterOutlet } from '@angular/router';
+import {Student } from './student/student'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Student],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/student/student.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  imports: [],
+  templateUrl: './student.html',
+  styleUrl: './student.css',
+})
+export class Student {
+  name='nessrine';
+  age=24;
+}
+
